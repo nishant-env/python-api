5fc7fba2f93c856953f006c69d651b338a67a9d9
Nishant <nishant.env@gmail.com>
2023-08-27 22:43:59 +0545

    Updated working with dbs

--- 5_working_with_db.py
+++ 5_working_with_db.py
@@ -5,11 +5,12 @@ from random import randint
 import psycopg2
 import time
 import pandas as pd
+from psycopg2.extras import RealDictCursor
 
 while True:
     try:
         conn = psycopg2.connect(database='fastapi', host='localhost', port='9876', user='postgres', password='Root@123')
-        cur = conn.cursor()
+        cur = conn.cursor(cursor_factory= RealDictCursor)
         break
     except Exception as e:
         print(e)
@@ -22,8 +23,7 @@ app = FastAPI()
 class PostModel(BaseModel):
     title: str
     content: str
-    published: Optional[bool] = False
-    rating: Optional[float] = None
+    published: Optional[bool] = True
 
 
 @app.get('/')
@@ -37,51 +37,54 @@ def all_posts():
         res = cur.fetchall()
     except:
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
-    return res    
+    return res
 
 @app.get('/posts/{id}')
 def single_post(id: int):
-    try:
-        cur.execute('select * from posts where id = %s' , str(id) )
-        res= cur.fetchall()
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    # try:
+        cur.execute('select * from posts where id = %s' , str(id))
+        res = cur.fetchall()
+        if len(res) == 0:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} is not found")
+    # except Exception as e:
+    #     print(e)
         # response.status_code=status.HTTP_404_NOT_FOUND
         # return {"message": f"post with id {id} was not found"}
-    return res
+        return {"data" : res}
 
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
+@app.post("/createposts", status_code=status.HTTP_201_CREATED)
 def new_post(post_inp: PostModel):
     try:
         post_dict = post_inp.dict()
-        cur.execute("""insert into posts(title,content,published) values (%s,%s,%s) returning *""", (post_dict["title"], post_dict["content"], post_dict["published"])) 
+        cur.execute("""insert into posts(title,content,published) values (%s,%s,%s) returning *""", (post_dict["title"], post_dict["content"], post_dict["published"]))
         post_ret = cur.fetchone()
         conn.commit()
     except Exception as e:
         print(e)
-        # raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)    
     return post_ret
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    try:
-        deletion_sql = f"delete from posts where id = {id}"
-        cur.execute(deletion_sql)
-        conn.commit()
-    except TypeError:
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Data with id {id} was not found")
+    deletion_sql = f"delete from posts where id = {id} returning *"
+    cur.execute(deletion_sql)
+    del_ret = cur.fetchone()
+    conn.commit()
+    if del_ret == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} is not found")
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 
-# @app.put("/posts/{id}")
-# def update_post(id: int, update_post: PostModel):
-#     try:
-#         dict_index = get_index(id)
-#         database[dict_index] = update_post.dict()
-#     except:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Data with id {id} was not found")
-#     return database[dict_index]
+@app.put("/updateposts/{id}")
+def update_post(id: int, update_post: PostModel):
+    cur.execute("update posts set title = %s, content = %s, published = %s where id = %s returning *", (update_post.title, update_post.content, update_post.published, id))
+    conn.commit()
+    updated_post = cur.fetchone()
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Data with id {id} was not found")
+
+    return updated_post
